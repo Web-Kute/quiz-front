c41b8822b72f561e9fcdb1f38f3c2a9e4bdf6905
Ikigai <alvardsandz@gmail.com>
2024-10-07 00:46:45 +0200

    add sessionStorage

--- dist/js/valid-form.js
+++ dist/js/valid-form.js
@@ -4,36 +4,121 @@ document.addEventListener('DOMContentLoaded', function () {
     var emailInput = document.getElementById('email');
     var errorName = document.getElementById('error-name');
     var errorEmail = document.getElementById('error-email');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        var isValid = true;
-        // Validate name
-        if (nameInput.value.length < 5 || nameInput.value.length > 25) {
-            if (errorName) {
-                errorName.textContent =
-                    'Le nom doit contenir entre 5 et 25 caractères.';
-                errorName.classList.remove('hide');
+    var NAME_TEXT_ERROR = 'Le nom doit contenir entre 5 et 25 caractères !';
+    var EMAIL_TEXT_ERROR = "Le format email n'est pas valide !";
+    var studentId = [];
+    var checkName = (nameInput === null || nameInput === void 0 ? void 0 : nameInput.value.length) < 5 ||
+        (nameInput === null || nameInput === void 0 ? void 0 : nameInput.value.length) >= 25 ||
+        /\d/.test(nameInput === null || nameInput === void 0 ? void 0 : nameInput.value) ||
+        (nameInput === null || nameInput === void 0 ? void 0 : nameInput.value) === '';
+    var connectOnLoad = JSON.parse(sessionStorage.getItem('loginId') || '[]');
+    if (nameInput && emailInput) {
+        nameInput.value = connectOnLoad[0] || '';
+        emailInput.value = connectOnLoad[1] || '';
+    }
+    function init() {
+        if (connectOnLoad.length > 0) {
+            if (nameInput) {
+                nameInput.value = '';
             }
-            isValid = false;
+            if (emailInput) {
+                emailInput.value = '';
+            }
+            studentId = [];
         }
-        else if (errorName) {
-            errorName.classList.add('hide');
+    }
+    init();
+    var emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    function validateName() {
+        var _a, _b, _c;
+        if (nameInput) {
+            checkName =
+                ((_a = nameInput === null || nameInput === void 0 ? void 0 : nameInput.value) === null || _a === void 0 ? void 0 : _a.length) < 5 ||
+                    ((_b = nameInput === null || nameInput === void 0 ? void 0 : nameInput.value) === null || _b === void 0 ? void 0 : _b.length) >= 25 ||
+                    /\d/.test((_c = nameInput === null || nameInput === void 0 ? void 0 : nameInput.value) !== null && _c !== void 0 ? _c : '') ||
+                    (nameInput === null || nameInput === void 0 ? void 0 : nameInput.value) === '';
         }
-        // Validate email
-        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (nameInput) {
+            if (checkName) {
+                if (errorName) {
+                    errorName.textContent = NAME_TEXT_ERROR;
+                    errorName.classList.replace('hide', 'show');
+                }
+            }
+            else {
+                if (errorName) {
+                    errorName.classList.replace('show', 'hide');
+                    nameInput.setAttribute('value', nameInput.value);
+                }
+            }
+        }
+    }
+    function validateEmail() {
+        if (emailInput === null) {
+            return;
+        }
         if (!emailRegex.test(emailInput.value)) {
             if (errorEmail) {
-                errorEmail.textContent = 'Veuillez entrer une adresse email valide.';
-                errorEmail.classList.remove('hide');
+                errorEmail.textContent = EMAIL_TEXT_ERROR;
+                errorEmail.classList.replace('hide', 'show');
             }
-            isValid = false;
         }
-        else if (errorEmail) {
-            errorEmail.classList.add('hide');
+        else {
+            if (errorEmail) {
+                errorEmail.classList.replace('show', 'hide');
+                emailInput.setAttribute('value', emailInput.value);
+            }
         }
-        if (isValid) {
-            console.log('Form is valid. Submitting...');
-            form.submit();
-        }
-    });
+    }
+    function handleError() {
+        document.addEventListener('click', function (e) {
+            var target = e.target;
+            if (target.tagName !== 'INPUT') {
+                if (errorName) {
+                    errorName.classList.replace('show', 'hide');
+                }
+                if (errorEmail) {
+                    errorEmail.classList.replace('show', 'hide');
+                }
+            }
+        });
+    }
+    handleError();
+    if (nameInput) {
+        nameInput.addEventListener('focus', validateName);
+        nameInput.addEventListener('keyup', validateName);
+    }
+    if (emailInput) {
+        emailInput.addEventListener('focus', validateEmail);
+        emailInput.addEventListener('keyup', validateEmail);
+    }
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            if (nameInput && emailInput) {
+                studentId.push(nameInput.value);
+                studentId.push(emailInput.value);
+            }
+            handleError();
+            checkName =
+                (nameInput === null || nameInput === void 0 ? void 0 : nameInput.value.length) < 5 ||
+                    (nameInput === null || nameInput === void 0 ? void 0 : nameInput.value.length) >= 25 ||
+                    /\d/.test(nameInput === null || nameInput === void 0 ? void 0 : nameInput.value) ||
+                    (nameInput === null || nameInput === void 0 ? void 0 : nameInput.value) === '';
+            sessionStorage.setItem('loginId', JSON.stringify(studentId));
+            if (checkName) {
+                if (errorName) {
+                    errorName.classList.replace('hide', 'show');
+                    errorName.textContent = NAME_TEXT_ERROR;
+                }
+            }
+            if (emailInput && !emailRegex.test(emailInput.value)) {
+                if (errorEmail) {
+                    errorEmail.classList.replace('hide', 'show');
+                    errorEmail.textContent = EMAIL_TEXT_ERROR;
+                }
+            }
+            init();
+        });
+    }
 });
--- src/valid-form.ts
+++ src/valid-form.ts
@@ -8,40 +8,138 @@ document.addEventListener('DOMContentLoaded', (): void => {
   const emailInput: HTMLInputElement | null = document.getElementById(
     'email',
   ) as HTMLInputElement;
+
   const errorName: HTMLElement | null = document.getElementById('error-name');
   const errorEmail: HTMLElement | null = document.getElementById('error-email');
 
-  form.addEventListener('submit', (event: Event): void => {
-    event.preventDefault();
-    let isValid: boolean = true;
+  const NAME_TEXT_ERROR = 'Le nom doit contenir entre 5 et 25 caractères !';
+  const EMAIL_TEXT_ERROR = "Le format email n'est pas valide !";
 
-    // Validate name
-    if (nameInput.value.length < 5 || nameInput.value.length > 25) {
-      if (errorName) {
-        errorName.textContent =
-          'Le nom doit contenir entre 5 et 25 caractères.';
-        errorName.classList.remove('hide');
+  let studentId: string[] = [];
+
+  let checkName =
+    nameInput?.value.length < 5 ||
+    nameInput?.value.length >= 25 ||
+    /\d/.test(nameInput?.value) ||
+    nameInput?.value === '';
+
+  const connectOnLoad = JSON.parse(sessionStorage.getItem('loginId') || '[]');
+
+  if (nameInput && emailInput) {
+    nameInput.value = connectOnLoad[0] || '';
+    emailInput.value = connectOnLoad[1] || '';
+  }
+
+  function init(): void {
+    if (connectOnLoad.length > 0) {
+      if (nameInput) {
+        nameInput.value = '';
       }
-      isValid = false;
-    } else if (errorName) {
-      errorName.classList.add('hide');
+      if (emailInput) {
+        emailInput.value = '';
+      }
+      studentId = [];
     }
+  }
+  init();
 
-    // Validate email
-    const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailRegex: RegExp =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  function validateName(): void {
+    if (nameInput) {
+      checkName =
+        nameInput?.value?.length < 5 ||
+        nameInput?.value?.length >= 25 ||
+        /\d/.test(nameInput?.value ?? '') ||
+        nameInput?.value === '';
+    }
+    if (nameInput) {
+      if (checkName) {
+        if (errorName) {
+          errorName.textContent = NAME_TEXT_ERROR;
+          errorName.classList.replace('hide', 'show');
+        }
+      } else {
+        if (errorName) {
+          errorName.classList.replace('show', 'hide');
+          nameInput.setAttribute('value', nameInput.value);
+        }
+      }
+    }
+  }
+
+  function validateEmail(): void {
+    if (emailInput === null) {
+      return;
+    }
     if (!emailRegex.test(emailInput.value)) {
       if (errorEmail) {
-        errorEmail.textContent = 'Veuillez entrer une adresse email valide.';
-        errorEmail.classList.remove('hide');
+        errorEmail.textContent = EMAIL_TEXT_ERROR;
+        errorEmail.classList.replace('hide', 'show');
       }
-      isValid = false;
-    } else if (errorEmail) {
-      errorEmail.classList.add('hide');
+    } else {
+      if (errorEmail) {
+        errorEmail.classList.replace('show', 'hide');
+        emailInput.setAttribute('value', emailInput.value);
+      }
     }
+  }
 
-    if (isValid) {
-      console.log('Form is valid. Submitting...');
-      form.submit();
-    }
-  });
+  function handleError(): void {
+    document.addEventListener('click', (e) => {
+      const target = e.target as HTMLElement;
+      if (target.tagName !== 'INPUT') {
+        if (errorName) {
+          errorName.classList.replace('show', 'hide');
+        }
+        if (errorEmail) {
+          errorEmail.classList.replace('show', 'hide');
+        }
+      }
+    });
+  }
+  handleError();
+
+  if (nameInput) {
+    nameInput.addEventListener('focus', validateName);
+    nameInput.addEventListener('keyup', validateName);
+  }
+
+  if (emailInput) {
+    emailInput.addEventListener('focus', validateEmail);
+    emailInput.addEventListener('keyup', validateEmail);
+  }
+
+  if (form) {
+    form.addEventListener('submit', (event: Event): void => {
+      event.preventDefault();
+      if (nameInput && emailInput) {
+        studentId.push(nameInput.value);
+        studentId.push(emailInput.value);
+      }
+      handleError();
+      checkName =
+        nameInput?.value.length < 5 ||
+        nameInput?.value.length >= 25 ||
+        /\d/.test(nameInput?.value) ||
+        nameInput?.value === '';
+
+      sessionStorage.setItem('loginId', JSON.stringify(studentId));
+
+      if (checkName) {
+        if (errorName) {
+          errorName.classList.replace('hide', 'show');
+          errorName.textContent = NAME_TEXT_ERROR;
+        }
+      }
+
+      if (emailInput && !emailRegex.test(emailInput.value)) {
+        if (errorEmail) {
+          errorEmail.classList.replace('hide', 'show');
+          errorEmail.textContent = EMAIL_TEXT_ERROR;
+        }
+      }
+      init();
+    });
+  }
 });
