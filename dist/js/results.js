2559ae4a3822163d28d9ec69041cfcd3ced6f556
Ikigai <alvardsandz@gmail.com>
2024-10-23 01:15:32 +0200

    add final score

--- dist/js/results.js
+++ dist/js/results.js
@@ -14,31 +14,31 @@ export function results(nbOfQuestions) {
       let correct = answered.filter(Boolean).length;
       const timerResults = Math.round(correct / answered.length) * 100;
       const total = Math.round((correct / nbOfQuestions) * 100);
-      // const total = elapsedTime ? timerResults : total;
+      const finalScore = elapsedTime === 0 ? timerResults : total;
       const student = userName ? `${userName}` : '';
       switch (true) {
-        case total <= 25.99:
-          notation.textContent = `Score : ${total} %`;
+        case finalScore <= 25.99:
+          notation.textContent = `Score : ${finalScore} %`;
           commentNotation.textContent = `Patience ${student}, les réponses vont venir !`;
           break;
-        case total >= 26 && total <= 50.99:
-          notation.textContent = `Score : ${total} % `;
+        case finalScore >= 26 && finalScore <= 50.99:
+          notation.textContent = `Score : ${finalScore} % `;
           commentNotation.textContent = `Pas si mal, ${student} croyez-en-vous !`;
           break;
-        case total >= 51 && total <= 75:
-          notation.textContent = `Score : ${total} % `;
+        case finalScore >= 51 && finalScore <= 75:
+          notation.textContent = `Score : ${finalScore} % `;
           commentNotation.textContent = `Bien, ${student} vous êtes en bonne voie !`;
           break;
-        case total >= 76 && total <= 87.5:
-          notation.textContent = `Score : ${total} % `;
+        case finalScore >= 76 && finalScore <= 87.5:
+          notation.textContent = `Score : ${finalScore} % `;
           commentNotation.textContent = `${student} Alors là ok, on peut parler`;
           break;
-        case total >= 87.6 && total <= 99.99:
-          notation.textContent = `Score : ${total} % `;
+        case finalScore >= 87.6 && finalScore <= 99.99:
+          notation.textContent = `Score : ${finalScore} % `;
           commentNotation.textContent = `Incroyable ${student} vous frôlez la perfection. Bravo !`;
           break;
-        case total === 100:
-          notation.textContent = `Score : ${total} % `;
+        case finalScore === 100:
+          notation.textContent = `Score : ${finalScore} % `;
           commentNotation.textContent = `Incroyable 100%, ${student} on étudie tout cela en laboratoire. Bravo !`;
           break;
         default:
