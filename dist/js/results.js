df88d32ba5b321f4c735e81cf731420e0bcc1583
Ikigai <alvardsandz@gmail.com>
2024-10-25 15:19:48 +0200

    nbsp result.js file

--- dist/js/results.js
+++ dist/js/results.js
@@ -38,7 +38,7 @@ export function results(nbOfQuestions) {
           break;
         case finalScore >= 76 && finalScore <= 87.5:
           notation.textContent = `Score : ${finalScore} % `;
-          commentNotation.textContent = `Très fort, alors là, ${student}, on peut parler`;
+          commentNotation.textContent = `Très fort, alors là, ${student}, on peut parler\u00a0!`;
           break;
         case finalScore >= 87.6 && finalScore <= 99.99:
           notation.textContent = `Score : ${finalScore} % `;
