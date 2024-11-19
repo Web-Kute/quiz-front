bc19986fb09514183e5e9d0437c8aae43269e0e8
Ikigai <alvardsandz@gmail.com>
2024-11-19 22:05:41 +0100

    fixed issues validation name

--- dist/js/login.js
+++ dist/js/login.js
@@ -7,94 +7,70 @@ document.addEventListener('DOMContentLoaded', () => {
   const emailRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-  const NAME_TEXT_ERROR = 'Le nom doit contenir entre 5 et 25 caractères !';
+  const NAME_TEXT_ERROR =
+    'Le nom doit contenir uniquement des lettres entre 5 et 25 caractères !';
   const EMAIL_TEXT_ERROR = "Le format email n'est pas valide !";
 
-  let isValid = false;
-
-  // const connectOnLoad = JSON.parse(sessionStorage.getItem('loginId') || '[]');
-  // name.value = connectOnLoad[0];
-  // email.value = connectOnLoad[1];
+  let nameValid = false;
+  let emailValid = false;
+  domElements.logged.value = false;
+  // let isValid = nameValid && emailValid;
 
   function validateName() {
     if (domElements.name === null) {
       return;
     }
-    // if (connectOnLoad) {
-    //   name.value = connectOnLoad[0];
-    // }
-    const checkName =
-      domElements.name.value.length < 5 ||
-      domElements.name.value.length >= 25 ||
-      /\d/.test(domElements.name.value ?? '') ||
-      domElements.name.value === '';
 
-    if (checkName && domElements.errorName) {
+    const nameValue = domElements.name.value;
+    const isNameLengthValid = nameValue.length >= 5 && nameValue.length < 25;
+    const hasNoDigits = !/\d/.test(nameValue);
+    const isNotEmpty = nameValue !== '';
+    const isNameInvalid = !(isNameLengthValid && hasNoDigits && isNotEmpty);
+
+    if (isNameInvalid && domElements.errorName) {
       domElements.errorName.textContent = NAME_TEXT_ERROR;
       domElements.errorName.classList.replace('hide', 'show');
-      isValid = false;
-    } else if (domElements.errorName) {
+      domElements.name.classList.remove('valid');
+      nameValid = false;
+    } else if (domElements.errorName && !isNameInvalid) {
       domElements.errorName.textContent = '';
       domElements.errorName.classList.replace('show', 'hide');
-      isValid = true;
+      domElements.name.classList.add('valid');
+      nameValid = true;
     }
   }
 
   function validateEmail() {
     if (domElements.email) {
-      const test =
-        domElements.email.value.length === 0 ||
-        emailRegex.test(domElements.email.value);
-      // email.value = connectOnLoad[1];
-      // Chaque fois que l'utilisateur saisit quelque chose
-      // on vérifie la validité du champ e-mail.
-      if (domElements.errorEmail && !test) {
+      const isValidEmail = emailRegex.test(domElements.email.value);
+      if (domElements.errorEmail && !isValidEmail) {
         domElements.errorEmail.textContent = EMAIL_TEXT_ERROR;
         domElements.errorEmail.classList.replace('hide', 'show');
-        isValid = false;
+        domElements.email.classList.remove('valid');
+        emailValid = false;
       } else {
         if (domElements.errorEmail) {
           domElements.errorEmail.textContent = '';
           domElements.errorEmail.classList.replace('show', 'hide');
-          isValid = true;
+          domElements.email.classList.add('valid');
+          emailValid = true;
         }
       }
     }
   }
-  if (domElements.form) {
-    domElements.form.addEventListener(
-      'submit',
-      function (event) {
-        // Chaque fois que l'utilisateur tente d'envoyer les données
-        // on vérifie que le champ email est valide.
-        console.log('logged', domElements.logged.value);
 
-        if (isValid) {
-          studentId.push(domElements.name.value);
-          studentId.push(domElements.email.value);
-          domElements.logged.value = true;
-        } else {
-          domElements.logged.value = false;
-        }
-
-        sessionStorage.setItem('loginId', JSON.stringify(studentId));
-        getLoginId = JSON.parse(sessionStorage.getItem('loginId'));
-
-        if (
-          domElements.email &&
-          domElements.errorEmail &&
-          !emailRegex.test(domElements.email.value)
-        ) {
-          // S'il est invalide, on affiche un message d'erreur personnalisé
-          domElements.errorEmail.innerHTML = EMAIL_TEXT_ERROR;
-          domElements.errorEmail.classList.replace('hide', 'show');
-          // Et on empêche l'envoi des données du formulaire
-          event.preventDefault();
-        }
-      },
-      false,
-    );
+  function isValidForm(event) {
+    event.preventDefault();
+    if (nameValid && emailValid) {
+      studentId.push(domElements.name.value);
+      studentId.push(domElements.email.value);
+      domElements.logged.value = true;
+      sessionStorage.setItem('loginId', JSON.stringify(studentId));
+      getLoginId = JSON.parse(sessionStorage.getItem('loginId'));
+      location.href = `choose.html?name=${domElements.name.value}&logged=${domElements.logged.value}`;
+    }
   }
+  domElements.form.addEventListener('submit', isValidForm);
 
   if (domElements.email) {
     domElements.email.addEventListener('focus', validateEmail);
